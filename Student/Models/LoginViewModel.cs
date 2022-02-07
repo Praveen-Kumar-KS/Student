db5585e03ed1cca4e3b75e45e980e0a6087b2150
Praveen-Kumar-KS <66106461+Praveen-Kumar-KS@users.noreply.github.com>
2022-02-07 16:32:46 +0530

    Models

---
+++ Student/Models/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Student.Models.Admin;
+using Student.Models.Student;
+using Student.Student.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Student.Models
+{
+    public class LoginViewModel
+    {
+        public int Id { get; set; }
+        public StudentViewModel StudentLogin { get; set; }
+        public AdminViewModel AdminLogin { get; set; }
+        public DataEnums LoginData { get; set; }
+
+    }
+}
---
+++ Student/Student.Data/Enums/DataEnums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Student.Student.Data.Enums
+{
+    public enum DataEnums
+    {
+        [Description("Admin")]
+        AdminLogin = 0,
+        [Description("Student")]
+        StudentLogin = 1
+    }
+}
